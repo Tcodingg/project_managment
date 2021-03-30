2bf6bf331de95f36fca0fd92f9e4cee40b433218
Tcodingg <32871055+Tcodingg@users.noreply.github.com>
2021-03-30 15:50:25 -0400

    reload page on submit success

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 const submit = document.getElementById('submit');
 
+let completed = false;
+
 burger.addEventListener('click', function () {
     menu.classList.toggle('show-menu')
     burger.classList.toggle('show-menu')
@@ -15,6 +17,7 @@ burger.addEventListener('click', function () {
 submit.addEventListener('click', (e) => {
     e.preventDefault();
     checkInput();
+    checkCompletion();
 
 })
 
@@ -52,19 +55,33 @@ function success(inputSelection) {
     formControl.classList.remove('error');
     formControl.classList.add('success');
 
-    const small = formControl.querySelector('small')
-    small.innerHTML = 'Success'
-    console.log('success', formControl)
+    const small = formControl.querySelector('small');
+    small.innerHTML = 'Success';
+    // console.log('success', formControl)
+    completed = true;
 }
 
 function error(inputSelection) {
     const formControl = inputSelection.parentElement;
 
-
     formControl.classList.remove('success');
     formControl.classList.add('error');
 
-    const small = formControl.querySelector('small')
+    const small = formControl.querySelector('small');
     small.innerHTML = 'Can not be blank';
-    console.log('error', formControl);
+    // console.log('error', formControl);
+    completed = false;
+}
+
+function checkCompletion() {
+    // completed ? console.log('completed') : console.log('there is an error')
+    if (completed === true) {
+
+        setTimeout(() => {
+
+            location.reload(true);
+        }, 300);
+    } else {
+        return;
+    }
 }
