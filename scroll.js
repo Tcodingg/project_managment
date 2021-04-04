e1f56a1967ce4e6042aed66052dbd064b33584f2
Tcodingg <32871055+Tcodingg@users.noreply.github.com>
2021-04-04 14:49:14 -0400

    add scroll effefct

---
+++ scroll.js
@@ -0,0 +1,11 @@
+$('.menu a').on('click', function (e) {
+    // console.log(this.hash);
+    if (this.hash !== '') {
+        e.preventDefault();
+        const hash = this.hash
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800)
+    }
+
+})
